bb8b528725282b370af25750dce856ae4edf682b
BosonHBC <a923000213@163.com>
2019-01-30 21:16:31 -0700

    Initial

---
+++ Gluttony/Assets/Scripts/BodyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    [SerializeField]
+    private float fMoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PerformSpeed();
+    }
+
+    void PerformSpeed()
+    {
+        float hori = Input.GetAxis("Horizontal");
+
+        rb.AddForce(transform.right * hori * fMoveSpeed, ForceMode.Force);
+    }
+}
